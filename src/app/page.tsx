6d27f3e4bef61e3daa9b6c3e826d595cff961656
USER <user@USERnoMacBook-Pro.local>
2025-09-06 15:34:17 +0900

    add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import { fetchGreeting } from "@/lib/data";
-import { Suspense } from "react";
+import Link from "next/link";
 
 export default async function Home() {
-  const res = await fetchGreeting();
-
   return (
-    <div>
-      <Suspense fallback={<div key={0}>loading...</div>}>
-        {res.map(data => (
-          <div key={data.id}>
-            <div>
-              <span>id: {data.id}</span>
-              <span>title: {data.title}</span>
-              <span>author: {data.author}</span>
-            </div>
-          </div>  
-        ))}
-      </Suspense>
-    </div>  
+    <main>
+      <h1>hogehoge</h1>
+      <Link href="/home">Goto Home!!</Link>
+    </main>
   );
 }
