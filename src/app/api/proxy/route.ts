5ac9993a30a2e0a9303fee749ca6bde8817d8012
USER <user@USERnoMacBook-Pro.local>
2025-09-07 15:54:25 +0900

    add console.log

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  console.log("api/proxy/route.ts")
   const api_url = process.env.NEXT_PUBLIC_API_URL;
+  console.log(`here route.ts, api_url: ${api_url}`)
   const res = await fetch(api_url + '/posts');
   const data = (await res.json());
 
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,10 +2,9 @@
 
 export const fetchGreeting = async() => {
   try {
-    console.log("lib/data.ts")
     const res = await fetch("http://localhost:3000/api/proxy", { cache: "no-store"});
     const _res = (await res.json()) as {id: number, title: string, author: string}[];
-    console.log(_res);
+    console.log(`here data.ts res: ${_res}`)
     return _res;
   } catch(e) {
     console.error(e)  
