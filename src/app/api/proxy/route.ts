2ca0a91deab4adc1548e87ab8d0e2d2dfec6ec0e
USER <user@USERnoMacBook-Pro.local>
2025-09-07 15:55:23 +0900

    typo

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const api_url = process.env.NEXT_PUBLIC_API_URL;
   console.log(`here route.ts, api_url: ${api_url}`)
-  const res = await fetch(api_url + '/posts');
+  const res = await fetch(api_url + '/post');
   const data = (await res.json());
 
   return NextResponse.json(data);
