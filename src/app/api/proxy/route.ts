f49edbbdea5bb92b3c00a52d8d7aacf694205775
USER <user@USERnoMacBook-Pro.local>
2025-09-07 15:44:42 +0900

    fix

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.log("api/proxy/route.ts")
   const api_url = process.env.NEXT_PUBLIC_API_URL;
   const res = await fetch(api_url + '/posts');
-  const data = (await res.json()) as {id: number, title: string, author: string}[];
+  const data = (await res.json());
 
   return NextResponse.json(data);
 }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,6 +2,7 @@
 
 export const fetchGreeting = async() => {
   try {
+    console.log("lib/data.ts")
     const res = await fetch("http://localhost:3000/api/proxy", { cache: "no-store"});
     const _res = (await res.json()) as {id: number, title: string, author: string}[];
     console.log(_res);
