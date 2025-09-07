1443c76df4bcf1f3e993375de393edd7a903420d
USER <user@USERnoMacBook-Pro.local>
2025-09-07 15:57:42 +0900

    add env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 };
 
 export default nextConfig;
